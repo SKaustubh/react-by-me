0d33fbf7861f8447bca4db97cfb7f17779124ee9
Kaustubh <kaustubhsingh37@gmail.com>
2023-12-08 00:09:19 +0530

    password generator using different hooks

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   const [color, setColor] = useState("olive");
 
   return (
+    
     <div
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
