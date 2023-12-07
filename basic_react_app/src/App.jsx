0cb6f07a3871671aecea604f27437e5187abd5b1
Kaustubh <kaustubhsingh37@gmail.com>
2023-12-07 22:06:32 +0530

    bg+tailwind importing

--- basic_react_app/src/App.jsx
+++ basic_react_app/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
+import "./index.css"
 
 function App() {
-  const [counter, setCounter] = useState(0);
 
-  let removeValue, addValue;
-  if (counter < 20) {
-    addValue = () => {
-      console.log("Clicked", Math.random(), counter);
-      setCounter(counter + 1);
-    };
-  }
+  const[color,setColor] = useState("oliver") 
 
-  if (counter > 0) {
-    removeValue = () => {
-      console.log("Removed", Math.random(), counter);
-      setCounter(counter - 1);
-    };
-  }
 
-  
-  return (
+  return(
     <>
-      <h1>my react</h1>
-      <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
-      <br />
-      <button onClick={removeValue}>remove value value</button>
+    <div className="w-full h-screen duration-200 " style={{ backgroundColor :color}}>h
+     
+    </div>
     </>
-  );
+  )
+   
+  
+
 }
 
 export default App;
---
+++ basic_react_app/src/Components/State_Eg_counter.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+
+function State_Eg_counter() {
+    const [counter, setCounter] = useState(0);
+
+    let removeValue, addValue;
+    if (counter < 20) {
+      addValue = () => {
+        console.log("Clicked", Math.random(), counter);
+        setCounter(counter + 1);
+      };
+    }
+  
+    if (counter > 0) {
+      removeValue = () => {
+        console.log("Removed", Math.random(), counter);
+        setCounter(counter - 1);
+      };
+    }
+  
+    
+    return (
+      <>
+        <h1>my react</h1>
+        <h2>Counter value: {counter}</h2>
+  
+        <button onClick={addValue}>Add value</button>
+        <br />
+        <button onClick={removeValue}>remove value value</button>
+      </>
+    );
+  }
+
+export default State_Eg_counter
