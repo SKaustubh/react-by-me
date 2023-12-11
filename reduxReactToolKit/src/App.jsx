836c3e9d9c8eb1c80ac32fbc550cfb1fbfdd6fbf
Kaustubh <kaustubhsingh37@gmail.com>
2023-12-11 18:46:23 +0530

    redux half added store and reducers

---
+++ reduxReactToolKit/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>redux react tool kit</h1>
+    </>
+  )
+}
+
+export default App
