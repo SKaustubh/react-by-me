691bf8706eefd11fc5f0e6b4aed8de80d33131ba
Kaustubh <kaustubhsingh37@gmail.com>
2024-01-16 21:05:22 +0530

    auth snippet for appwrite

---
+++ megaBlog/src/appwrite/auth.js
@@ -0,0 +1,72 @@
+// appwrite services snippet
+
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client)
+
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.Login({ email, password });
+            }
+            else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.error("Create Account", error);
+            throw error;
+        }
+    }
+
+    async Login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+
+        } catch (error) {
+            console.error("Login error :", error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service : : getCurrentUser : : error", error);
+
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+
+        } catch (error) {
+            console.error("Appwrite service :: Logout error :", error);
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService;
+
+
+// Remove the unnecessary export statement
+// export default AuthService;
