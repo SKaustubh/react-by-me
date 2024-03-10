975288259e8f8d098d0ffdec778db5ddd37d3d0a
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-11 00:33:22 +0530

    logo implemented

--- megaBlog/src/components/Logo.jsx
+++ megaBlog/src/components/Logo.jsx
@@ -1,11 +1,15 @@
-
-
-function Logo({width="w-10",height="h-10"}) {
+function Logo({ width = "w-10", height = "h-10" }) {
   return (
     <div>
-      <div>Logo</div>
+      <div>
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/4576/4576683.png"
+          alt="Logo"
+          className={`${width} ${height}`}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
