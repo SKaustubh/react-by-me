c126303e139c06eb4da071508a4b254d3ce3ea0a
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-10 13:34:57 +0530

    pages created

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -4,11 +4,31 @@ import Container from "./container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import Post from "../pages/Post";
+import PostForm from "./post-form/PostForm";
+import PostCards from "./PostCards"
+import EditPost from "../pages/EditPost";
+import Home from "../pages/Home";
+import AllPosts from "../pages/AllPosts";
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    AddPost,
+    Post,
+    PostForm,
+    PostCards,
+    EditPost,
+    Home,
+    AllPosts,
+    AuthLayout
 };
